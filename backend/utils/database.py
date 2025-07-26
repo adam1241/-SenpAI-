50ca16d11717137bafdf64f2f217d9200009c629
Zahdi <ZahdiMohcine20@gmail.com>
2025-07-26 14:53:36 +0200

    upgrading the database file

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1,13 +1,23 @@
-
-
+import json
+import os
 
 class Database:
-    
-    def __init(self):
+    def __init__(self):
         self.tables = {
             "flash_cards": "./database/flash_cards.json"
         }
 
     def load_table(self, table_name):
-        with open(self.tables[table_name], "r") as f:
-            return json.load(f)
+        if os.path.exists(self.tables[table_name]):
+            with open(self.tables[table_name], "r") as f:
+                return json.load(f)
+        return []
+
+    def add_to_table(self, table_name, data):
+        # Load existing data
+        existing_data = self.load_table(table_name)
+        # Append new data
+        existing_data.append(data)
+        # Write back the complete list
+        with open(self.tables[table_name], "w") as f:
+            json.dump(existing_data, f, indent=2)
